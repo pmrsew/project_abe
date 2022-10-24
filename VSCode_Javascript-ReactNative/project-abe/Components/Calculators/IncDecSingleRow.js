2b531b98661d5b591025f25bd6f984a20ae341bc
Paige <paige0011@gmail.com>
2022-10-23 21:36:25 -0500

    added math logic for calculator, not yet tested

--- VSCode_Javascript-ReactNative/project-abe/Components/Calculators/IncDecSingleRow.js
+++ VSCode_Javascript-ReactNative/project-abe/Components/Calculators/IncDecSingleRow.js
@@ -10,15 +10,6 @@ import ChoiceButton from "../Buttons/ChoiceButton";
 
 export default function IncDecCalc() {
     const [incDec, setIncDec] = useState('inc')
-
-    function decClick() {
-        setIncDec('dec')
-    }
-
-    function incClick() {
-        setIncDec('inc')
-    }
-
     const [startCount, setStartCount] = useState('')
     const [stitchDif, setStitchDif] = useState('')
 
@@ -39,11 +30,57 @@ export default function IncDecCalc() {
     const [result, setResult] = useState('Result will display here after calculation')
 
     function calcClick() {
-        setResult('Calc has been clicked')
+        //setResult('Calc has been clicked')
+
+        const stStart = parseInt(startCount)
+        const stDif = parseInt(stitchDif)
+        const stEnd = endCount()
+        //will need catches for invalid cases:
+            //empty entry
+            //zero entry
+            //decimal entry
+            //negative endCount
+        
+        //amount of stitches from the starting row used in a single repetition
+        const repLength = stStart/stDif
+        let resultString = ''
+
+        if(stStart%stDif === 0){
+            //amount of single stitches in a row per repetition
+            //inc uses 1 stitch from starting row
+            //dec uses 2 stitches from starting row
+            const stRep = (incDec==='inc'?(repLength-1):(repLength-2))
+
+            //since there's a clean division, the amount of stitches being inc/dec is the amount of repetitions needed
+            if(incDec==='inc'){
+                resultString = `[[stitch] into next ${stRep} stitch(es), 2[stitch] into next] ${stDif} time(s) (${stEnd})`
+            }else{
+                resultString = `[[Stitch] into next ${stRep} stitch(es), dec] ${stDif} time(s) (${stEnd})`
+            }
+        }else{
+            //when division isn't clean, there will be two repetition patterns
+            //first rep will match previously used formula
+            const stRepA = (incDec==='inc'?(repLength-1):(repLength-2))
+            //second rep will use one more stitch from the starting row
+            //this prevents there being too many inc/dec when accounting for mod amount
+            //and keeps inc/dec pretty even across the row
+            const stRepB = (incDec==='inc'?(repLength):(repLength-1))
+            //a single repetition now uses more stitches from the starting row
+            const repCount = stStart/((repLength*2) + 1)
+            //since division isn't clean, there will be remaining stitches to add after repetitions
+            const stRemain = stStart%((repLength*2) + 1)
+
+            if(incDec==='inc'){
+                resultString = `[[stitch] into next ${stRepA} stitch(es), 2[stitch] into next, [stitch] into next ${stRepB} stitch(es), 2[stitch] into next] ${repCount} time(s), [stitch] into next ${stRemain} (${stEnd})`
+            }else{
+                resultString = `[[Stitch] into next ${stRepA} stitch(es), dec, [Stitch] into next ${stRepB} stitch(es), dec] ${repCount} time(s), [stitch] into next ${stRemain} (${stEnd})`
+            }
+        }
+        setResult(resultString)
     }
 
     function copyClick() {
-
+        
     }
 
     function resetClick() {
@@ -59,9 +96,9 @@ export default function IncDecCalc() {
             <Text style={styles.sectionHeader}>Calculator for Single Increases and Decreases</Text>
             <View style={incDecCalcStyles.contentContainer}>
                 <View style={styles.multiButtonRow}>
-                    <ChoiceButton pressFunc={incClick} btnText='Increase' active={incDec === 'inc' ? true : false} />
+                    <ChoiceButton pressFunc={()=>setIncDec('inc')} btnText='Increase' active={incDec === 'inc' ? true : false} />
                     <View style={{ width: 10 }} />
-                    <ChoiceButton pressFunc={decClick} btnText='Decrease' active={incDec === 'dec' ? true : false} />
+                    <ChoiceButton pressFunc={()=>setIncDec('dec')} btnText='Decrease' active={incDec === 'dec' ? true : false} />
                 </View>
 
                 <Text>Starting Stitch Count</Text>
